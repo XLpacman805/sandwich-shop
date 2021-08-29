09d4655599400280782687383f6eef060030814e
XLpacman805 <xlpacman805@gmail.com>
2021-08-28 21:31:05 -0700

    Created the unit tests and storybook

--- frontend/src/components/atoms/CloseButton/CloseButton.tsx
+++ frontend/src/components/atoms/CloseButton/CloseButton.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import propTypes from './propTypes';
 
-const Button = styled.button`
-    background-color: #ffffff;
-    color: #0070eb;
+const Button = styled.button<propTypes>`
+    background-color: ${props => (props ? props.defaultBackgroundColor : "#fffffff")};
+    color: ${props => (props ? props.defaultColor : "#0070eb")};
     border-style: none;
     border-radius: 50%;
     width: 30px;
@@ -18,20 +19,25 @@ const Button = styled.button`
     &:hover,
     &:focus,
     &:active {
-        color: #003f85;
-    }
+        color: ${props => (props.altColor ? props.altColor : "#003f85")};
 
     &:active {
-        background-color: #d4d4db;
+        background-color: ${props => (props.altBackgroundColor ? props.altBackgroundColor : "#d4d4db")};
     }
-
 `;
 
 //create a functional component
-const CloseButton = (props : any) => {
+const CloseButton = (props : propTypes) => {
     // return a button
     return (
-        <Button data-testid={props.dataTestId} className="close-button" onClick={props.onClick}>
+        <Button data-testid={props.dataTestId} 
+            className="close-button" 
+            defaultBackgroundColor={props.defaultBackgroundColor}
+            defaultColor={props.defaultColor}
+            altColor={props.altColor}
+            altBackgroundColor={props.altBackgroundColor}
+            onClick={props.onClick}>
+
             <span>&times;</span>
         </Button>
     );
